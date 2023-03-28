5b61bcbe0213052c5285214a18969e1a8960d71e
Ondrej Hanak <524918@mail.muni.cz>
2023-03-28 14:59:28 +0200

    buggy 3 fixes

--- buggy_python_code.py
+++ buggy_python_code.py
@@ -1,5 +1,3 @@
-import sys 
-import os
 import yaml
 import flask
 
@@ -28,11 +26,11 @@ def fetch_website(urllib_version, url):
     exec(f"import urllib{urllib_version} as urllib", globals())
     # Fetch and print the requested URL
  
-    try: 
+    """try: 
         http = urllib.PoolManager()
         r = http.request('GET', url)
     except:
-        print('Exception')
+        print('Exception')"""
 
 
 def load_yaml(filename):
@@ -52,7 +50,8 @@ if __name__ == '__main__':
     print("3. Yaml deserialization vulnerability: use string=file.yaml")
     print("4. Use of assert statements vulnerability: run program with -O argument")
     choice  = input("Select vulnerability: ")
-    if choice == "1": 
+    
+    if choice == "1":
         new_person = Person("Vickie")  
         print_nametag(input("Please format your nametag: "), new_person)
     elif choice == "2":
