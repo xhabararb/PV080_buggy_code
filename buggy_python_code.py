d1c17a44b952e689ec5b7c18cd9a7f934c2a8c39
Ondrej Hanak <524918@mail.muni.cz>
2023-03-28 15:26:03 +0200

    bruh

--- buggy_python_code.py
+++ buggy_python_code.py
@@ -23,7 +23,7 @@ def print_nametag(format_string, person):
 
 def fetch_website(urllib_version, url):
     # Import the requested version (2 or 3) of urllib
-    if urlib_version in (2,3):
+    if urllib_version in (2,3):
         exec(f"import urllib{urllib_version} as urllib", globals())
     # Fetch and print the requested URL
  
